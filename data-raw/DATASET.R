e96aaf08c8c5e1e11e356adbb6dcc0fa202af658
albhasan <albhasan@gmail.com>
2024-09-13 11:05:12 -0300

    Reference to name column updated.

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -5,7 +5,10 @@
 library(maps)
 
 countries_sf <- rnaturalearth::ne_countries(scale = "small")
+countries_sf <- countries_sf["name"]
+
 states_sf    <- rnaturalearth::ne_states()
+states_sf    <- states_sf["name"]
 
 usethis::use_data(countries_sf, states_sf, overwrite = TRUE)
 
